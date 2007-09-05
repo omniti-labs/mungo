345368009e6126e90a323c696bc4e4574b9a3192
Theo Schlossnagle <jesus@lethargy.org>
2007-09-05 15:08:09 +0000

    reset the file to the beginning when we are finished building it, closes #4

--- lib/Mungo/MultipartFormData.pm
+++ lib/Mungo/MultipartFormData.pm
@@ -24,6 +24,7 @@ sub new {
           $part->{handle} = IO::Scalar->new(\$part->{payload});
           delete $part->{payload};
         }
+        $part->{handle}->seek(0,0);
       }
       else {
         $self->{$part->{name}} = $part->{payload};
